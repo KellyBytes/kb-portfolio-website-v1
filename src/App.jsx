3ae200aac7814cfd7782ddb8990ea826883270a0
Kelly <kellybytes.ca@gmail.com>
2025-10-06 20:06:41 -0600

    Fix light/dark mode switching

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from './context/ThemeContext';
 const App = () => {
   return (
     <ThemeProvider>
-      <div className="w-full min-h-screen text-[var(--c-primary)]">
+      <div className="w-full min-h-screen text-[var(--c-primary)] bg-gradient-to-br from-slate-50 via-slate-100 to-slate-200 dark:from-gray-800 dark:via-gray-900 dark:to-gray-950">
         <NavBar />
         <div className="pt-56 lg:pt-0 lg:mt-0 lg:ml-[45%]">
           <Hero />
