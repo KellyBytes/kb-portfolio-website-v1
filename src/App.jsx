f0a248f01fcab6be1604a03d634173361f5593db
Kelly <kellybytes.ca@gmail.com>
2025-10-06 19:53:42 -0600

    Fix lihgt/dark mode switching

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import { ThemeProvider } from './context/ThemeContext';
 const App = () => {
   return (
     <ThemeProvider>
-      <div className="w-full min-h-screen bg-linear-to-br from-[var(--bg-primary)] from-5% via-[var(--bg-secondary)] via-35% to-[var(--bg-tertiary)] text-[var(--c-primary)]">
+      <div
+        style={{ background: 'var(--gradient-bg)' }}
+        className="w-full min-h-screen text-[var(--c-primary)]"
+      >
         <NavBar />
         <div className="pt-56 lg:pt-0 lg:mt-0 lg:ml-[45%]">
           <Hero />
